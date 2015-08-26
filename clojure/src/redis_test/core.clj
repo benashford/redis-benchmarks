b9a11d117a949239abdc1716429381b02017d3c7
Ben Ashford <benashford@users.noreply.github.com>
2015-08-26 20:59:19 +0100

    A more realistic use-case, better than 1000 pings

--- clojure/src/redis_test/core.clj
+++ clojure/src/redis_test/core.clj
@@ -1,19 +1,48 @@
 (ns redis-test.core
-  (:require [redis-async.core :as redis-async]
+  (:require [clojure.core.async :as a]
+            [redis-async.core :as redis-async]
             [redis-async.client :as client]
             [taoensso.carmine :as car]))
 
-(def ^:private cmds 1000)
+;; shared
 
-(defn test-redis-async [p]
-  (let [last-c (last (repeatedly cmds #(client/ping p)))]
-    (client/<!! last-c)))
+(def ^:private cmds 10000)
+
+(defn- generate-random-string []
+  (str (rand-int 50000) "-" (rand-int 50000) "-" (rand-int 50000)))
+
+(defn- generate-test-data []
+  (into [] (repeatedly cmds generate-random-string)))
+
+;; redis-async
+
+(defn- a-populate [p test-data]
+  (a/go-loop [[data & rest-of-data] test-data
+              id-c                  (client/incr p "ID-GEN-KEY")]
+    (let [id (client/<! id-c)]
+      (client/set p (str "key:" id) data)
+      (client/rpush p "QUEUE" id))
+    (when rest-of-data
+      (recur rest-of-data (client/incr p "ID-GEN-KEY")))))
+
+(defn test-a [p test-data]
+  (client/flushdb p)
+  (a/<!! (a-populate p test-data)))
+
+;; carmine
 
 (defmacro wcar* [& body] `(car/wcar {:pool {} :spec {}} ~@body))
 
-(defn test-carmine-pipe []
-  (last (wcar* (dotimes [i cmds]
-                 (car/ping)))))
+(defn- b-populate [test-data]
+  (loop [[data & rest-of-data] test-data
+         id                    (wcar* (car/incr "ID-GEN-KEY"))]
+    (let [[_ _ next-id] (wcar*
+                         (car/set (str "key:" id) data)
+                         (car/rpush "QUEUE" id)
+                         (car/incr "ID-GEN-KEY"))]
+      (when rest-of-data
+        (recur rest-of-data next-id)))))
 
-(defn test-carmine []
-  (last (map (fn [_] (car/wcar {:pool {} :spec {}} (car/ping))) (range cmds))))
+(defn test-b [test-data]
+  (wcar* (car/flushdb))
+  (b-populate test-data))
